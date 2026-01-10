e1e7a7a9130b12d63c863c9ea92ea49d1dec1e0d
Davide Libenzi <davide.libenzi@gmail.com>
2026-01-10 07:54:41 +0100

    Added package namespace.

--- macros.sv
+++ macros.sv
@@ -1,9 +1,12 @@
-let MAX(A, B) = ((A > B) ? A : B);
-let MIN(A, B) = ((A > B) ? B : A);
-let ABS(A) = (($signed(A) >= 0) ? A : -$signed(A));
-let FABS(A) = ((A >= 0.0) ? A : -A);
 
-let EXP_OFFSET(NX) = (2**(NX - 1) - 1);
+package fp;
+  let MAX(A, B) = ((A > B) ? A : B);
+  let MIN(A, B) = ((A > B) ? B : A);
+  let ABS(A) = (($signed(A) >= 0) ? A : -$signed(A));
+  let FABS(A) = ((A >= 0.0) ? A : -A);
+
+  let EXP_OFFSET(NX) = (2**(NX - 1) - 1);
+endpackage
 
 // This in theory should be a typedef within the FPU interface, but then
 // many HDL tools do not support hierarchical type dereferencing.
